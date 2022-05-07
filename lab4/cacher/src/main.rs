bc5595412567f302b161ea20f6f3d3bf0a639e81
Ковалевский Вячеслав Максимович <kovalevskij.vm@edu.spbstu.ru>
2022-05-07 15:08:17 +0300

    it works

--- lab4/cacher/src/main.rs
+++ lab4/cacher/src/main.rs
@@ -1,11 +1,25 @@
+use std::{sync::mpsc::channel, thread};
+
+use handler::run;
 use postgres::{Client, NoTls, Error};
-use requests::{row_to_string, Entity};
+use requests::random_select;
 
 mod handler;
 mod requests;
 
 fn main() -> Result<(), Error> {
     let mut client = Client::connect("host=localhost user=postgres dbname=railway", NoTls)?;
-    
+
+    let (request_tx, request_rx) = channel();
+    let (response_tx, response_rx) = channel();
+
+    thread::spawn(move || {
+        run(client, request_rx, response_tx, 8);
+    });
+
+    request_tx.send(random_select());
+    let response = response_rx.recv().unwrap();
+    println!("{response}");
+
     return Ok(())
 }
